109823a92f4efcdcc70ac1d3cfca1607244a0a37
molingyuan <linarielfour@gmail.com>
2025-04-24 21:06:46 +0800

    新增頁面(通知、聊天室、個人檔案)

---
+++ trading_platform/lib/screens/chat_list.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ChatList extends StatelessWidget {
+  const ChatList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('聊天列表頁面'), // 之後要換成真正的聊天列表
+    );
+  }
+}
